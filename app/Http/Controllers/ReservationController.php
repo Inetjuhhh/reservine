a7024ca200162aceb3e8568169870d654eafb601
Ine <ine@inetjuhhh.nl>
2025-05-12 16:31:45 +0200

    get reservations of today or later

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function index()
+    {
+        $reservations = \App\Models\Reservation::with('guest', 'table')
+            ->whereDate('starts_at', '>=', Carbon::today())->get();
+        return view('dashboard')->with('reservations', $reservations);
+    }
 }
